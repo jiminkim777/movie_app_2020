aec8b39afde230ecaaa6bc50f324dcf429dc49a6
jimin <jm9062@naver.com>
2020-12-02 01:36:10 +0900

    https://github.com/jiminkim777/movie_app_2020

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+
+function Food({name, pic, rat}){
+  return(
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rat} / 5</h4>
+      <img src={pic} alt={name}/>
+    </div>
+  ); 
+}
+
+Food.propTypes = {
+  name:PropTypes.string.isRequired,
+  pic:PropTypes.string.isRequired,
+  rat:PropTypes.number.isRequired
+}
+
+const foodILike = [
+  {
+    id:1,
+    name:"Kimchi",
+    image:
+    "https://image.edaily.co.kr/images/photo/files/NP/S/2018/11/PS18112701515.jpg",
+    rating:4.9
+  },
+  {
+    id:2,
+    name:"ramen",
+    image:
+    "http://img.hani.co.kr/imgdb/resize/2012/0806/8000971505_20120806.JPG",
+    rating:4.3
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+   {foodILike.map(dish =>(
+     <Food key={dish.id} name={dish.name} pic={dish.image} rat={dish.rating}/>     
+   ))}
+  
+  </div>  
+  );  
 }
 
 export default App;
+
